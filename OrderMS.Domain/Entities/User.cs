4a68f6166f9da9740160791cc89ef3b159e3d826
ttadesse627 <ttadesse627@gmail.com>
2025-11-25 21:49:39 +0300

    Added properties for user entity

--- OrderMS.Domain/Entities/User.cs
+++ OrderMS.Domain/Entities/User.cs
@@ -1,6 +1,11 @@
-
+using Microsoft.AspNetCore.Identity;
 
 namespace OrderMS.Domain.Entities
 {
-    public class User : IdentityUser
+    public class User : IdentityUser<Guid>
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string? Address { get; set; }
+    }
 }
