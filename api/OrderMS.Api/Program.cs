906fc1f5dcedcbf0338248ec3c92c1134392a32e
ttadesse627 <ttadesse627@gmail.com>
2025-11-28 16:23:32 +0300

    Feature: Added the cors policy for incoming requests

--- api/OrderMS.Api/Program.cs
+++ api/OrderMS.Api/Program.cs
@@ -1,25 +1,47 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using OrderMS.Api.Middlewares;
+using OrderMS.Application;
 using OrderMS.Infrastructure;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
+string allowSpecificOrigins = "_allowSpecificoOrigins";
+
 builder.Services.AddProblemDetails(configure =>
 {
     configure.CustomizeProblemDetails = context =>
     {
-        context.ProblemDetails.Extensions.TryAdd("RequestId", context.HttpContext.TraceIdentifier);
+        context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
     };
 });
+
 builder.Services.AddExceptionHandler<ValidationExceptionHandler>();
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = null;
+    });
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: allowSpecificOrigins, policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+    });
+});
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddApplicationServices()
+                .AddInfrastructureServices(builder.Configuration);
+
 builder.Services.AddOpenApi();
 
 WebApplication app = builder.Build();
@@ -28,13 +50,17 @@ WebApplication app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseExceptionHandler();
 app.UseHttpsRedirection();
+
+app.UseCors(allowSpecificOrigins);
 
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
