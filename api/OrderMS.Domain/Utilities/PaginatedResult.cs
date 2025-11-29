b83bda8401a1563c02e5a04d099b3cbe06a3e048
ttadesse627 <ttadesse627@gmail.com>
2025-11-29 14:46:28 +0300

    Feature: Added the Pagination model for item list

---
+++ api/OrderMS.Domain/Utilities/PaginatedResult.cs
@@ -0,0 +1,11 @@
+
+namespace OrderMS.Domain.Utilities;
+
+public class PaginatedResult<T>
+{
+    public IReadOnlyList<T> Items { get; set; } = [];
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+    public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+}
