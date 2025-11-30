b521c05f09ef7b71153763353220af333d619c01
ttadesse627 <ttadesse627@gmail.com>
2025-11-30 10:22:36 +0300

    Fix: Updated token generation technique

--- api/OrderMS.Infrastructure/Persistence/TokenGeneratorService.cs
+++ api/OrderMS.Infrastructure/Persistence/TokenGeneratorService.cs
@@ -1,10 +1,8 @@
-
-
-using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
 using Microsoft.IdentityModel.Tokens;
 using OrderMS.Application.Services;
 using OrderMS.Domain.Entities;
@@ -17,28 +15,30 @@ public class TokenGeneratorService(IOptions<JwtSettings> options, UserManager<Ap
     private readonly JwtSettings _jwtSettings = options.Value;
     private readonly UserManager<ApplicationUser> _userManager = userManager;
 
-    public string GenerateToken(ApplicationUser user)
+    public async Task<string> GenerateTokenAsync(ApplicationUser user)
     {
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var secretKey = Encoding.ASCII.GetBytes(_jwtSettings.SecretKey ?? throw new InvalidOperationException("JWT Key not configured."));
+        var secretKey = Encoding.ASCII.GetBytes(_jwtSettings.SecretKey ??
+                        throw new InvalidOperationException("JWT Key not configured."));
 
-        var userRoles = _userManager.GetRolesAsync(user).Result;
+        var userRoles = await _userManager.GetRolesAsync(user);
 
-        string joinedRoles = string.Join(",", userRoles);
+        List<Claim> claims = [
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email ?? string.Empty)
+        ];
+
+        claims.AddRange(userRoles.Select(ur => new Claim(ClaimTypes.Role, ur)));
         var tokenDescriptor = new SecurityTokenDescriptor
         {
-            Subject = new ClaimsIdentity(
-            [
-                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-                new Claim(ClaimTypes.Email, user.Email ?? string.Empty),
-                new Claim(ClaimTypes.Name, user.UserName ?? string.Empty),
-                new Claim(ClaimTypes.Role, joinedRoles),
-            ]),
+            Subject = new ClaimsIdentity(claims),
             Expires = DateTime.UtcNow.AddMinutes(_jwtSettings.ExpiryInMinutes),
-            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(secretKey), SecurityAlgorithms.HmacSha256Signature)
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(secretKey), SecurityAlgorithms.HmacSha256),
+            Issuer = _jwtSettings.Issuer,
+            Audience = _jwtSettings.Audience
         };
-
+        var tokenHandler = new JsonWebTokenHandler();
         var token = tokenHandler.CreateToken(tokenDescriptor);
-        return tokenHandler.WriteToken(token);
+
+        return await Task.FromResult(token);
     }
 }
