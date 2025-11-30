9fd22f651f0ff5325bffe737b04bb398728a0cba
ttadesse627 <ttadesse627@gmail.com>
2025-11-30 10:23:23 +0300

    Fix: Updated the common response

--- api/OrderMS.Application/Dtos/Responses/ApiResponse.cs
+++ api/OrderMS.Application/Dtos/Responses/ApiResponse.cs
@@ -2,4 +2,10 @@
 
 namespace OrderMS.Application.Dtos.Responses;
 
-public record ApiResponse<T>(T? Data, bool Success = false, string? Message = null);
+public record ApiResponse<T>
+{
+    public T? Data { get; set; }
+    public string? Message { get; set; }
+    public List<string> Errors { get; set; } = [];
+    public bool Success { get; set; } = false;
+}
