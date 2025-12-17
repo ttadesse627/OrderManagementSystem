b6b477e5adab3457f4294f2b79fd52961192f78e
ttadesse627 <ttadesse627@gmail.com>
2025-12-17 17:43:45 +0300

    Feature: Added Redis for fast in memory token storage

--- api/OrderMS.Infrastructure/ServiceContainer.cs
+++ api/OrderMS.Infrastructure/ServiceContainer.cs
@@ -5,12 +5,14 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.JsonWebTokens;
 using Microsoft.IdentityModel.Tokens;
 using OrderMS.Application.Services;
 using OrderMS.Domain.Entities;
 using OrderMS.Domain.Utilities;
 using OrderMS.Infrastructure.Persistence;
 using OrderMS.Infrastructure.Services;
+using StackExchange.Redis;
 
 namespace OrderMS.Infrastructure;
 
@@ -28,7 +30,10 @@ public static class ServiceContainer
     public static IServiceCollection ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("PostgresqlConnection")
-            ?? throw new InvalidOperationException("Connection string 'OrderMSConnectionString' not found.");
+            ?? throw new InvalidOperationException("Connection string for PostgreSQL not found.");
+
+        var redisConnectionString = configuration.GetConnectionString("RedisConnection")
+            ?? throw new InvalidOperationException("Connection string for Redis not found.");
 
         services.AddDbContext<ApplicationDbContext>(options =>
         {
@@ -41,8 +46,26 @@ public static class ServiceContainer
             options.UseNpgsql(connectionString);
         }, ServiceLifetime.Scoped);
 
+        var remoteRedisConnection = Environment.GetEnvironmentVariable("REDIS_CONNECTION")
+            ?? throw new InvalidOperationException("Redis Connection not found from .env");
+
+        services.AddSingleton<IConnectionMultiplexer>(sp =>
+        {
+            var options = ConfigurationOptions.Parse(redisConnectionString);
+            options.AbortOnConnectFail = false;
+            options.ConnectRetry = 5;
+            options.ConnectTimeout = 5000;
+
+            Console.WriteLine($"Parsed Redis Connection : {options}");
+
+            return ConnectionMultiplexer.Connect(options);
+        });
+
+        Console.WriteLine($"Redis Connection from Environment Variable: {remoteRedisConnection}");
+
         return services;
     }
+
     public static IServiceCollection ConfigureIdentity(this IServiceCollection services, IConfiguration configuration)
     {
         var jwtSettingsSection = configuration.GetSection("JwtSettings");
@@ -60,7 +83,8 @@ public static class ServiceContainer
             options.Password.RequiredLength = 6;
         })
         .AddRoles<IdentityRole<Guid>>()
-        .AddEntityFrameworkStores<ApplicationDbContext>();
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders();
 
         services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(jwtOptions =>
@@ -75,6 +99,25 @@ public static class ServiceContainer
                     ClockSkew = jwtSettings.ExpiryInMinutes > 0
                         ? TimeSpan.FromMinutes(jwtSettings.ExpiryInMinutes)
                         : TimeSpan.Zero
+                };
+
+                jwtOptions.Events = new JwtBearerEvents
+                {
+                    OnTokenValidated = async context =>
+                    {
+                        var jti = context.Principal?.FindFirst(JwtRegisteredClaimNames.Jti)?.Value;
+                        if (string.IsNullOrEmpty(jti))
+                        {
+                            context.Fail("Unknown token.");
+                            return;
+                        }
+
+                        var revokationService = context.HttpContext.RequestServices.GetRequiredService<ITokenRepository>();
+                        if (await revokationService.IsTokenRevokedAsync(jti))
+                        {
+                            context.Fail("This token has been revoked prreviously.");
+                        }
+                    }
                 };
             });
 
@@ -91,12 +134,13 @@ public static class ServiceContainer
         services.AddScoped<IIdentityService, IdentityService>();
         services.AddScoped<IUserResolverService, UserResolverService>();
         services.AddTransient<ITokenGeneratorService, TokenGeneratorService>();
+        services.AddTransient<ITokenRepository, TokenRepository>();
 
         services.AddTransient(typeof(ICommonRepository<>), typeof(CommonRepository<>));
         services.AddTransient<IProductRepository, ProductRepository>();
         services.AddTransient<IFileService, FileService>();
+        services.AddTransient<IFileRepository, FileRepository>();
         services.AddTransient<ICategoryRepository, CategoryRepository>();
-
         services.AddTransient<IOrderRepository, OrderRepository>();
         services.AddTransient<ICustomerRepository, CustomerRepository>();
 
